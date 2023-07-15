e1471dd6779d7f3a4c1a45694d59ce0c04929132
Wyatt Conrad <wyatt.conrad@gmail.com>
2023-07-15 09:08:10 -0400

    Updated database string

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,7 @@ using Api;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ApiIsolated
 {
@@ -9,8 +10,8 @@ namespace ApiIsolated
     {
         public static void Main()
         {
-            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PtSLManager_db-2023-7-13;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
+            //var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PtSLManager_db-2023-7-13;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection");
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
                 .ConfigureServices(services =>
